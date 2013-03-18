2898d2d898725429fa68899bbd2b6cdf0b5be803
ninja <mamasic@gmail.com>
2013-03-18 17:20:19 +0100

    Trgovanje dan datum i komentar

---
+++ CoolJ/DatabaseGeneric/BusinessLogic/TrgovanjeMjesec.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using NinjaSoftware.Api.CoolJ;
+using NinjaSoftware.TrzisteNovca.CoolJ.HelperClasses;
+using NinjaSoftware.TrzisteNovca.CoolJ.EntityClasses;
+
+namespace NinjaSoftware.TrzisteNovca.CoolJ.DatabaseGeneric.BusinessLogic
+{
+    public class TrgovanjeMjesec
+    {
+        public ValutaEnum Valuta { get; set; }
+        public int Mjesec { get; set; }
+        public decimal? Potraznja { get; set; }
+        public decimal? Promet { get; set; }
+        public decimal? KamatnaStopa { get; set; }
+
+        public static List<TrgovanjeMjesec> GetTrgovanjeMjesecCollection(DataAccessAdapterBase adapter, int godina, ValutaEnum valutaEnum)
+        {
+            DateTime startDate = new DateTime(godina, 1, 1);
+            DateTime endDate = new DateTime(godina, 12, 31);
+
+            RelationPredicateBucket bucket = new RelationPredicateBucket();
+            bucket.Relations.Add(TrgovanjeStavkaEntity.Relations.TrgovanjeGlavaEntityUsingTrgovanjeGlavaId);
+
+            bucket.PredicateExpression.Add(TrgovanjeGlavaFields.Datum >= startDate);
+            bucket.PredicateExpression.Add(TrgovanjeGlavaFields.Datum <= endDate);
+
+            EntityCollection<TrgovanjeStavkaEntity> trgovanjeStavkaCollection = TrgovanjeStavkaEntity.FetchTrgovanjeStavkaCollection(adapter, bucket, null);
+
+            IEnumerable<long> valutaIdCollection = trgovanjeStavkaCollection.Select(ts => ts.ValutaId).Distinct();
+
+            foreach (long valutaId in valutaIdCollection)
+            { 
+            
+            }
+        }
+    }
+}
---
+++ NinjaSoftware.TrzisteNovca/Models/Home/PrometGodina.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NinjaSoftware.TrzisteNovca.CoolJ.DatabaseGeneric.BusinessLogic;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace NinjaSoftware.TrzisteNovca.Models.Home
+{
+    public class PrometGodina
+    {
+        #region Constructors
+
+        public PrometGodina(DataAccessAdapterBase adapter, int? godina)
+        {
+            if (!godina.HasValue)
+            {
+                godina = DateTime.Now.Year;
+            }
+
+
+        }
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+    }
+
+    public class PrometGodinaRecord
+    {
+        public ValutaEnum Valuta { get; set; }
+        public string Mjesec { get; set; }
+        public decimal? Potraznja { get; set; }
+        public decimal? Promet { get; set; }
+        public decimal? KamatnaStopa { get; set; }
+    }
+}
